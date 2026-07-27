48ed95527131383b5bc6d496bea638cc2ececc65
Sven Pfennig <sven.pfennig@hetzner-cloud.de>
2026-07-27 16:37:29 +0200

    fix: deprecation warnings, moved to rand 0.9 API

--- crates/controller-manager/src/controllers/deployment.rs
+++ crates/controller-manager/src/controllers/deployment.rs
@@ -1208,7 +1208,7 @@ impl<S: Storage + 'static> DeploymentController<S> {
                             Ok(_) => break,
                             Err(rusternetes_common::Error::Conflict(_)) => {
                                 if attempt + 1 < 3 {
-                                    let jitter = rand::thread_rng().gen_range(0..delay_ms);
+                                    let jitter = rand::rng().random_range(0..delay_ms);
                                     tokio::time::sleep(Duration::from_millis(delay_ms + jitter))
                                         .await;
                                     delay_ms = (delay_ms * 2).min(500);
